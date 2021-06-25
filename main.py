727c9a69eeb6b74fd3580ad28810ce13b714e3b6
Jeff Chiu <jwchiu82@gmail.com>
2021-06-25 14:05:49 -0700

    add ascii art and start on TDL

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+from art import logo
+
+#1: pick a random number, save as a variable
+#2: pick difficulty level, assign variable to determine number of guesses
+#3: while loop to continue game until guess is made OR out of guesses e.g. game_over = False
